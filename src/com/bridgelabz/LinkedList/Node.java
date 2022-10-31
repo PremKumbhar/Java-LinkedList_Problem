191892853a2ddd087caa28579a63c43008dfc8fd
PremKumbhar <kumbharprem7738@gmail.com>
2022-10-31 12:41:28 +0530

    [Prem]:Added simple linkedlist

---
+++ src/com/bridgelabz/LinkedList/Node.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.LinkedList;
+
+public class Node<T>{
+	T data;
+	Integer s;
+	
+	Node<T> next;
+	
+	public Node() {
+		
+	}
+	public void compare() {
+		
+	}
+	public Node(T data) {
+		this.data = data;
+	}
+	
+	
+}
